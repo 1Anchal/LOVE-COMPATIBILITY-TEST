b0b836cc3a5958e03ae2434b458debc8e3d27c6d
Anchal Kumari <162045321+1Anchal@users.noreply.github.com>
2024-07-28 22:09:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+function calculateLove() {
+    
+    var myName = document.getElementById("myName").value;
+    var partnersName = document.getElementById("partnersName").value;
+    var calculate = document.getElementsByClassName("calculate");
+    
+    var combinedName = myName.toLowerCase() + partnersName.toLowerCase();
+    
+    console.log(combinedName);
+
+    
+    let a=0;
+    for(let i=0;i<combinedName.length;i++){
+        if(combinedName[i]==="t"||combinedName[i]==="r"||combinedName[i]==="u"||combinedName[i]==="e"){
+            a=a+1;
+        }
+    }
+    let b=0;
+    for(let i=0;i<combinedName.length;i++){
+        if(combinedName[i]==="t"||combinedName[i]==="r"||combinedName[i]==="u"||combinedName[i]==="e"){
+            b=b+1;
+        }
+    }
+
+    
+
+
+    let score=a.toString()+b.toString();
+    console.log(score);
+    var output = document.querySelector(".output");
+
+    // Update the output based on the score
+    if (score >= 89|| score <= 11 && score!=0) {
+        if(score<=11){
+            score=100-score;
+        }
+        output.innerText = `Your score is ${score}. You both are TRUE LOVERS and made for each other!!`;
+        output.style.color = "green";
+    } else if (score < 90 && score >= 60) {
+        output.innerText = `Your score is ${score}. You both are compatible.`;
+        output.style.color = "green";
+    } else {
+        output.innerText = `Your score is ${score}. You can find someone better.`;
+        output.style.color = "red";
+    }
+}
+document.querySelector('.calculate').addEventListener('click', calculateLove);
